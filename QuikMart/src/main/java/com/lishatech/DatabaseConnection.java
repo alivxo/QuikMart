14a9fbd34f96dd6453b800d93ba9b0fe8b75f815
alivxo <avsal0218@gmail.com>
2024-11-06 20:13:42 -0500

    connected code base to database

---
+++ QuikMart/src/main/java/com/lishatech/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package com.lishatech;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/SushQuikMart";
+        String username = "root";
+        String password = "YUm15510n";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Oops, there was an error connecting to the database.");
+            e.printStackTrace();
+        }
+
+
+    }
+}
