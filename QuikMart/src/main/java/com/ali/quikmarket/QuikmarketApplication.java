9a03f940a22ffff982384cbe2cf0f2ed4922185d
alivxo <avsal0218@gmail.com>
2024-11-14 13:49:52 -0500

    Springboot commit

---
+++ QuikMart/src/main/java/com/ali/quikmarket/QuikmarketApplication.java
@@ -0,0 +1,13 @@
+package com.ali.quikmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuikmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuikmarketApplication.class, args);
+	}
+
+}
